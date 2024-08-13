90bd93f6ff1636b51ed838d935c5941ab22bac34
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-08-13 22:30:21 +0700

    Fixing Shape Detection

--- ShapeDetection/shape_detection.py
+++ ShapeDetection/shape_detection.py
@@ -3,94 +3,46 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
-
 def shape_detection():
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    image_path = os.path.join(script_dir, 'image.png')
+    object_path = os.path.join(script_dir, './images/object.jpg')
+    scene_path = os.path.join(script_dir, './images/scene.jpg')
 
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    gray = np.float32(gray)
+    img_object = cv2.imread(object_path)
+    img_scene = cv2.imread(scene_path)
 
-    harris_result = cv2.cornerHarris(gray, 2, 5, 0.04)
+    surf = cv2.SIFT_create()
 
-    cv2.imshow('Harris', harris_result)
-    cv2.waitKey(0)
+    kp_object, des_object = surf.detectAndCompute(img_object, None)
+    kp_scene, des_scene = surf.detectAndCompute(img_scene, None)
 
-    without_subpix = img.copy()
-    without_subpix[harris_result > 0.01*harris_result.max()] = [0, 0, 255]
+    des_object = des_object.astype('f')
+    des_scene = des_scene.astype('f')
 
-    _, thresh = cv2.threshold(harris_result, 0.01*harris_result.max(), 255, 0)
-    thresh = np.uint8(thresh)
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE)
+    search_params = dict(checks=50)
 
-    _, _, _, corner_centroids = cv2.connectedComponentsWithStats(thresh)
-    corner_centroids = np.float32(corner_centroids)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-    criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 100, 0.001)
+    matches = flann.knnMatch(des_object, des_scene, 2)
 
-    enhanced_corners = cv2.cornerSubPix(gray, corner_centroids, (2, 2), (-1, -1), criteria)
+    matchesMask = []
+    for i in range(0, len(matches)):
+        matchesMask.append([0, 0])
 
-    with_subpix = img.copy()
-    corner_centroids = np.int16(corner_centroids)
-    for centroid in corner_centroids:
-        centroid_y = centroid[1]
-        centroid_x = centroid[0]
+    total_match = 0
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            matchesMask[i] = [1, 0]
+            total_match += 1
 
-        with_subpix[centroid_y, centroid_x] = [0, 0, 255]
-        
-    enhanced_corners = np.int16(enhanced_corners)
-    for corner in enhanced_corners:
-        corner_y = corner[1]
-        corner_x = corner[0]
-        
-        with_subpix[corner_y, corner_x] = [0, 255, 0]
-
-    plt.figure(1, figsize=(7, 7))
-
-    without_subpix = cv2.cvtColor(without_subpix, cv2.COLOR_BGR2RGB)
-    plt.subplot(1, 2, 1)
-    plt.imshow(without_subpix)
-    plt.title('Without subpix')
-    plt.xticks([])
-    plt.yticks([])
-
-    with_subpix = cv2.cvtColor(with_subpix, cv2.COLOR_BGR2RGB)
-    plt.subplot(1, 2, 2)
-    plt.imshow(with_subpix)
-    plt.title('With subpix')
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.show()
-
-    fast = cv2.FastFeatureDetector_create()
-    fast_keypoints = fast.detect(img, None)
-    fast_result = img.copy()
-    cv2.drawKeypoints(img, fast_keypoints, fast_result, color=[0, 0, 255])
-    fast_result = cv2.cvtColor(fast_result, cv2.COLOR_BGR2RGB)
-
-    orb = cv2.ORB_create()
-    orb_keypoints = orb.detect(img, None)
-    orb_result = img.copy()
-    cv2.drawKeypoints(img, orb_keypoints, orb_result, color=[255, 0, 0])
-    orb_result = cv2.cvtColor(orb_result, cv2.COLOR_BGR2RGB)
-
-    combined_result = img.copy()
-    cv2.drawKeypoints(img, fast_keypoints, combined_result, color=[0, 0, 255]) 
-    cv2.drawKeypoints(combined_result, orb_keypoints, combined_result, color=[255, 0, 0]) 
-
-    combined_result = cv2.cvtColor(combined_result, cv2.COLOR_BGR2RGB)
-
-    plt.subplot(1, 2, 1)
-    plt.imshow(fast_result)
-    plt.title('FAST Keypoints (Red)')
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.subplot(1, 2, 2)
-    plt.imshow(orb_result)
-    plt.title('ORB Keypoints (Blue)')
-    plt.xticks([])
-    plt.yticks([])
-
+    img_res = cv2.drawMatchesKnn(
+        img_object, kp_object, img_scene,
+        kp_scene, matches, None,
+        matchColor = [0, 255, 0], 
+        singlePointColor = [255, 0, 0], 
+        matchesMask = matchesMask
+    )
+    plt.imshow(img_res)
     plt.show()
